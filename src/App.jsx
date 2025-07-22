fd06f8a1cfd0bba116102b50eea00b2a21aab762
aya alaa <aya-28-10@hotmail.com>
2025-07-22 19:40:42 +0300

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/Pizza-Hut">
       <Header />
       <Routes>
         <Route path='/' element={<Home/>}/>
